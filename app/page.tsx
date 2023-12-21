3d5faf5ed4e9dc58f86b3c399aa1f3f3d02ee5de
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-12-21 15:30:49 -0800

    Created Route group structure

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next.js 14 we're coming</h1>
-      <h2 className="h2-bold">Testing</h2>
-      <h3 className="h3-bold">Testing</h3>
-    </div>
-  );
-}
