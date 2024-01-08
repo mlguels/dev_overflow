44d239abc64fec6c3bf5c2b16032c5635cdc6047
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-01-07 23:44:55 -0800

    Changed some props to be optional

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -6,8 +6,8 @@ import { Badge } from "@/components/ui/badge";
 interface Props {
   _id: string;
   name: string;
-  totalQuestions: number;
-  showCount: boolean;
+  totalQuestions?: number;
+  showCount?: boolean;
 }
 
 const RenderTag = ({ _id, name, totalQuestions, showCount }: Props) => {
