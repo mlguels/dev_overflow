ac6331af737f987ece410c6b1a814cbd8768e499
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-01-08 00:28:03 -0800

    Updated question section on the homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
@@ -8,30 +9,38 @@ import Link from "next/link";
 
 const questions = [
   {
-    _id: 1,
+    _id: "1",
     title: "Cascading Deletes in SQLAlchemy",
     tags: [
-      { _id: 1, name: "python" },
-      { _id: 2, name: "sql" },
+      { _id: "1", name: "python" },
+      { _id: "2", name: "sql" },
     ],
-    author: "John Doe",
-    upvotes: 10,
-    views: 100,
-    answers: 5,
-    createdAt: "2021-09-11T12:34:00.000Z",
+    author: {
+      _id: "1",
+      name: "John Doe",
+      picture: "URL_TO_PICTURE",
+    },
+    upvotes: 1500000,
+    views: 500000,
+    answers: [],
+    createdAt: new Date("2023-09-11T12:34:00.000Z"),
   },
   {
-    _id: 1,
+    _id: "2",
     title: "How to center a div",
     tags: [
-      { _id: 1, name: "css" },
-      { _id: 2, name: "react" },
+      { _id: "3", name: "css" },
+      { _id: "4", name: "react" },
     ],
-    author: "John Doe",
+    author: {
+      _id: "1",
+      name: "John Doe",
+      picture: "URL_TO_PICTURE",
+    },
     upvotes: 10,
     views: 100,
-    answers: 5,
-    createdAt: "2021-09-11T12:34:00.000Z",
+    answers: [],
+    createdAt: new Date("2022-09-11T12:34:00.000Z"),
   },
 ];
 
@@ -68,7 +77,19 @@ export default function Home() {
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {questions.length > 0 ? (
-          questions.map((question) => "QuestionsCard")
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
         ) : (
           <NoResult
             title="There are no questions to show"
