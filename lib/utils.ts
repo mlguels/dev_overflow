737e9d6bd9c055a4f4cdaeb2ea3d35ccf2d204fa
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-01-08 00:27:36 -0800

    Created 2 utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,38 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getTimeStamp = (createdAt: Date): string => {
+  const now: Date = new Date();
+  const timeDifference: number = now.getTime() - createdAt.getTime();
+
+  const seconds: number = Math.floor(timeDifference / 1000);
+  const minutes: number = Math.floor(seconds / 60);
+  const hours: number = Math.floor(minutes / 60);
+  const days: number = Math.floor(hours / 24);
+
+  if (days > 0) {
+    return `${days} day${days === 1 ? "" : "s"} ago`;
+  } else if (hours > 0) {
+    return `${hours} hour${hours === 1 ? "" : "s"} ago`;
+  } else if (minutes > 0) {
+    return `${minutes} minute${minutes === 1 ? "" : "s"} ago`;
+  } else {
+    return `${seconds} second${seconds === 1 ? "" : "s"} ago`;
+  }
+};
+
+export const formatAndDivideNumber = (num: number): string => {
+  if (num >= 1000000) {
+    const formattedNum = (num / 1000000).toFixed(1);
+    return `${formattedNum}M`;
+  } else if (num >= 1000) {
+    const formattedNum = (num / 1000).toFixed(1);
+    return `${formattedNum}K`;
+  } else {
+    return num.toString();
+  }
+};
